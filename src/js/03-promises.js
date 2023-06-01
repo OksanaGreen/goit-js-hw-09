df9b6d5cc66857b2d52dd83f44530474f3e9fff9
OksanaGreen <Springocsana@ukr.net>
2023-06-01 15:20:05 +0300

    eeee

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  startBtn: document.querySelector('button'),
+  form: document.querySelector('form'),
+  //  перша затримка
+  delayInput: document.querySelector('input[name="delay"]'),
+  // кожна наступна
+  stepInput: document.querySelector('input[name="step"]'),
+  // кількість промісів
+  amountInput: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+        // Fulfill
+      } else {
+        reject({ position, delay });
+        // Reject
+      }
+    }, delay);
+  });
+}
+
+refs.startBtn.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const delay = Number(refs.delayInput.value);
+  const step = Number(refs.stepInput.value);
+  const amount = Number(refs.amountInput.value);
+  console.log(delay, step, amount);
+
+  startBtn.disabled = true;
+
+  if (delay < 0 || step < 0 || amount < 0) {
+    Notify.warning(`Enter number more than 0`);
+  } else if (Number(amount) === 0) {
+    Notify.warning(`Enter number more than 0`);
   } else {
-    // Reject
+    for (let i = 0; i < amount; i + 1) {
+      createPromise(i, delay + step * i)
+        .then(({ position, delay }) => {
+          console.log({ position, delay });
+          Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log({ position, delay });
+          Notify.failure(`❌ Rejected promise ${position + 1} in ${delay}ms`);
+        });
+    }
   }
 }
